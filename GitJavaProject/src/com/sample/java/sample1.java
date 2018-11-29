6270f1ec049a90cf4eb154955b77be66423831dd
venkatb <venkatb@WSHYD-031.WILCO.COM>
2018-11-29 12:49:38 +0530

    v1

---
+++ GitJavaProject/src/com/sample/java/sample1.java
@@ -0,0 +1,13 @@
+package com.sample.java;
+
+public class sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("v1");
+		
+		
+	}
+
+}
